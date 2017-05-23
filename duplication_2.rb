1924bbfa95e893acc7e95f55e77e3b671f3e4df4
Devon Blandin <dblandin@gmail.com>
2017-05-22 20:14:01 -0400

    Add duplication issues

---
+++ duplication_2.rb
@@ -0,0 +1,30 @@
+require "securerandom"
+
+class AnotherExample
+  def identical(message, header: true)
+    body = []
+    body << "-- HEADER --" if header
+    body << "-- MESSAGE --"
+    body << message
+    body << "-- FOOTER --"
+    message = body.join("\n")
+
+    if message.length > 100
+      message[0, 100]
+    else
+      message
+    end
+  end
+
+  def similar
+    hex = SecureRandom.hex(20)
+    hex.reverse
+    hex += "-suffix"
+
+    if hex.start_with?("a")
+      hex += "something"
+    end
+
+    hex
+  end
+end
