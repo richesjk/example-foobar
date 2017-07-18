161dd9fdce4d32ebe9bfbc964c4615c7ae64c8f3
Will Fleming <will@flemi.ng>
2017-07-18 14:42:21 -0400

    Update foo_bar

--- foo_bar.rb
+++ foo_bar.rb
@@ -1,1 +1,39 @@
-# TODO: Add FooBar content
+class FooBar
+  # Perform performs perform
+  def perform!
+    if ((a && b) || ((c || d) && (e || f)) || g)
+      puts "this was true"
+    else
+      puts "this was false"
+    end
+  end
+
+  def long_method(i)
+    case i
+    when 1
+      return "Hello!"
+    when 2
+      return "Hi!"
+    when 3
+      return "Bonjour!"
+    when 4
+      return "Guten tag!"
+    when 5
+      return "Ciao!"
+    when 6
+      return "Alright?"
+    when 7
+      return "Greetings..."
+    when 8
+      return "... and Salutations"
+    when 9
+      return "Sup."
+    when 10
+      return "Hola!"
+    when 11
+      return "Namaste!"
+    else
+      raise ArgumentError, "didn't expect that"
+    end
+  end
+end
