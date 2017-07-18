0ed49b7c5885eaa5ea5f9e09b2f80d6919078a67
Will Fleming <will@flemi.ng>
2017-07-18 14:42:21 -0400

    Update foo_bar

--- duplication_2.rb
+++ duplication_2.rb
@@ -1,1 +1,7 @@
-# TODO: Add duplication_2.rb content
+class Duplicated
+  def impl_two(a, b, c, d)
+    e = a + b + c + d / ( b * d / a) * (a / c) - (a * b * d * c * 100)
+    f = e * 10 - a - d * c * 7
+    puts "f is #{f}"
+  end
+end
