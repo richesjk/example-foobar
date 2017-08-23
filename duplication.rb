a95c7c0f35884890c8d20b57aabf2eb04e9cbdec
Noah Portes Chaikin <nporteschaikin@codeclimate.com>
2017-08-23 10:26:09 -0400

    Too many return statements

--- duplication.rb
+++ duplication.rb
@@ -3,4 +3,15 @@ class Duplicated
   def impl_one(a, b, c, d)
     a + b + c + d / ( b * d / a) * (a / c) - (a * b * d * c * 100)
   end
+
+  def impl_two
+    return true
+    return true
+    return true
+    return true
+    return true
+    return true
+    return true
+    return true
+  end
 end
